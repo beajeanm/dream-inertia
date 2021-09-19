0ca751fb0a4836511e6d7489de4d57f6b3781b0b
Jean-Michel Bea <beajeanm@users.noreply.github.com>
2021-09-19 14:00:08 +0100

    Use crunch to serve the compiled assets (#4)

--- example/src/example.ml
+++ example/src/example.ml
@@ -3,17 +3,43 @@ let inertia =
     ~base_url:Uri.(of_string "http://localhost:8080")
     ~version:None ~template:Index.render ()
 
+let crunch_build_loader _directory path _request =
+  match Assets.Build.read path with
+  | Some data ->
+      Dream.respond data
+  | None ->
+      Dream.empty `Not_Found
+
+let crunch_static_loader _directory path _request =
+  match Assets.Static.read path with
+  | Some data ->
+      Dream.respond data
+  | None ->
+      Dream.empty `Not_Found
+
 let () =
+  let _ = Assets.Build.file_list in
   let content = Yojson.Safe.from_string {json|
   { "name" : "world"}
   |json} in
   let data = Dream_inertia.{component= "Home"; props= content} in
-  let handler = Dream_inertia.inertia_handler inertia (fun _req -> data) in
+  let intertia_handler =
+    Dream_inertia.inertia_handler inertia (fun _req -> data)
+  in
   Dream.run ~adjust_terminal:false
   @@ Dream.logger
+  @@ Dream_livereload.(
+       inject_script
+       (* We are running webpack, then crunch before the ocaml build, so we need
+          a bit more time to reload *)
+         ~script:(default_script ~retry_interval_ms:1000 ~max_retry_ms:10000 ())
+         ())
   @@ Dream.router
-       [ Dream.get "/" handler
+       [ Dream.get "/" intertia_handler
+       ; Dream_livereload.route ()
        ; Dream.get "/favicon.png"
          @@ Dream.from_filesystem "public" "favicon.png"
-       ; Dream.get "/build/**" @@ Dream.static "public/build/" ]
+       ; Dream.get "/build/**" @@ Dream.static ~loader:crunch_build_loader ""
+       ; Dream.get "/static/**" @@ Dream.static ~loader:crunch_static_loader ""
+       ]
   @@ Dream.not_found
